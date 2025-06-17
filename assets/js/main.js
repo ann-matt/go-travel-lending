80794282eb773d41880d4f7d80c9634c6cc6cc51
Ann Matt <anmatyushkova@gmail.com>
2025-06-17 18:34:57 +0300

    try js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,37 @@ import '../js/modalOne.js'
 import '../js/scrollHeaders.js'
 import '../js/testimonialSwiper.js'
 import '../js/circleText.js'
+
+const navMenu = document.querySelector('.modal-one__container');
+const navToggle = document.querySelector('.nav__toggle');
+const navClose = document.getElementById('modal-close');
+
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+});
+
+navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+});
+
+document.querySelector('.modal-one').addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal-one')) {
+        navMenu.classList.remove('show-menu');
+    }
+})
+
+const swiperHome = new Swiper('.home__swiper', {
+
+  loop: true,
+  slidesPerView: 'auto',
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  }
+});
